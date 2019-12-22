2aaa4ed1f227d12f3f3647d0e7422e5823c2e690
Heung <coca0105@naver.com>
2019-12-22 23:03:49 +0900

    finished chapter 11

---
+++ Assets/Work/Chapter11/Scripts/HeatmapDrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatmapDrawer : MonoBehaviour
+{
+    public Vector4 [] positions;
+    public float [] radiuses;
+    public float [] intensities;
+    public Material material;
+
+    void Update ()
+    {
+        material.SetInt ("_Points_Length", positions.Length);
+        material.SetVectorArray ("_Points", positions);
+
+        Vector4 [] properties = new Vector4 [positions.Length];
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            properties [i] = new Vector2 (radiuses [i], intensities [i]);
+        }
+
+        material.SetVectorArray ("_Properties", properties);
+    }
+}
